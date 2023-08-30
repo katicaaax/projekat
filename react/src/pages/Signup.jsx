6052ea24c2adf6ddd9b281f91812b40ad81b3a8b
katicaaax <radosavljevic.katarina98@gmail.com>
2023-08-30 21:41:31 +0200

    Signup forma - referenciranje unetih podataka.

--- react/src/pages/Signup.jsx
+++ react/src/pages/Signup.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
-
+import {useRef} from "react";
 export default function Signup() {
 
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
 
 const onSubmit = (ev) => {
     ev.preventDefault()
+    const payload = {
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        passwordRef: passwordRef.current.value,
+        password_confirmation: passwordConfirmationRef.current.value,
+
+    }
+    console.log(payload);
 }
 
 
@@ -15,10 +27,10 @@ const onSubmit = (ev) => {
                 <h1 className="title">
                     Signup for free
                 </h1>
-    <input placeholder="Full Name" />
-    <input type="email" placeholder="Email Address" />
-    <input type="password" placeholder="Password" />
-    <input type="password" placeholder="Password Confirmation" />
+    <input ref = {nameRef} placeholder="Full Name" />
+    <input ref = {emailRef} type="email" placeholder="Email Address" />
+    <input ref = {passwordRef} type="password" placeholder="Password" />
+    <input ref = {passwordConfirmationRef} type="password" placeholder="Password Confirmation" />
     <button className="btn btn-block">Signup</button>
     <p className="message">
     Already Registered? <Link to="/login">Login</Link>
