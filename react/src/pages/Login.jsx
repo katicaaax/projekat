95da5367af898ba2975d1e1ee65ba2b44ed54c4e
katicaaax <radosavljevic.katarina98@gmail.com>
2023-08-30 15:50:20 +0200

    Kreiranje forme za login/signup. Prvi deo.

--- react/src/pages/Login.jsx
+++ react/src/pages/Login.jsx
@@ -1,7 +1,18 @@
 export default function Login() {
 
+const onSubmit = (ev) => {
+ev.preventDefault()
+}
+
     return (
-        <div>
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+<input type="email" placeholder="Email" />
+<input type="password" placeholder="Password" />
+<button className="brn brn-block"></button>
+                </form>
+            </div>
             Login
         </div>
     )
