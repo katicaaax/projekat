00f1799374d2d0596a488ed3e03f300a8683b925
katicaaax <radosavljevic.katarina98@gmail.com>
2023-08-30 15:31:28 +0200

    DefaultLayout - CSS i rutiranje.

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,6 +1,6 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
-import {Navigate} from "react-router-dom";
+
 
 
 export default function DefaultLayout() {
@@ -12,9 +12,24 @@ const {user, token} = useStateContext()
   }
   
   return (
-<div>
-  
+<div id="defaultLayout">
+    <aside> 
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Users</Link>
+    </aside>
+    <div className="content">
+        <header>
+            <div>
+                Header
+            </div>
+      <div>
+        User info
+    </div>
+    </header>
+<main>
     <Outlet />
+</main>
+</div>
 </div>
 
     )
