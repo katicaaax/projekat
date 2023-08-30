113f9bc150f3558202064c727ed431ce63ec6957
katicaaax <radosavljevic.katarina98@gmail.com>
2023-08-30 22:23:57 +0200

    Signup Request - definicija pravila

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -11,7 +11,7 @@ class SignupRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,17 @@ class SignupRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:55',
+            'email' => 'required|email|unique:users,email', 
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)
+                ->letters()
+                ->symbols()
+                ->numbers()
+
+            ]
         ];
     }
 }
