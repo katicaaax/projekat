dd15da2eeaece3b65b37596b475aee71453c9c1f
katicaaax <radosavljevic.katarina98@gmail.com>
2023-08-30 15:16:53 +0200

    Zasticene rute (bez tokena). Drugi deo.

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import {Navigate} from "react-router-dom";
+
 
 export default function DefaultLayout() {
 
 const {user, token} = useStateContext()
   
-  if(!token){
+  if(!token){ //ako user nije logovan, preusmeri ga na loginpage
     return <Navigate to ="/login" />
   }
   
--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+import { Navigate } from "react-router-dom";
 
 export default function GuestLayout() {
+    const {token} = useStateContext()
+    if(token) {
+        return <Navigate to="/"/>
+    }
+
 
     return ( 
-//renderujemo login i signup
 <div>
 
 <Outlet />   
