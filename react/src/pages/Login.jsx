386242bcda41da74119c190f2fce7f6edfe85f53
katicaaax <radosavljevic.katarina98@gmail.com>
2023-08-31 21:14:55 +0200

    Implementacija Logina. Prvi deo.

--- react/src/pages/Login.jsx
+++ react/src/pages/Login.jsx
@@ -2,19 +2,46 @@ import { Link } from "react-router-dom";
 
 export default function Login() {
 
+    const emailRef = createRef()
+    const passwordRef = createRef()
+    const {setUser, setToken} = useStateContext()
+    const [errors, setErrors] = useState(null)
+
 const onSubmit = (ev) => {
 ev.preventDefault()
+const payload = {
+    email: emailRef.current.value,
+    password: passwordRef.current.value,
+
+  }
+  axiosClient.post( '/login', payload)
+    .then(({data}) => {
+      setUser(data.user)
+      setToken(data.token);
+    })
+    .catch(err => {
+      const response = err.response;
+      if (response && response.status === 422) {
+        setErrors(response.data.errors)
+      }
+    })
 }
+
 
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit}>
-                    <h1 className="titlee=">
-                        Login into your account
-                    </h1>
-<input type="email" placeholder="Email" />
-<input type="password" placeholder="Password" />
+                <h1 className="title">Login into your account</h1>
+          {errors &&
+            <div className="alert">
+              {Object.keys(errors).map(key => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          }
+<input ref={emailRef} type="email" placeholder="Email" />
+<input ref={passwordRef} type="password" placeholder="Password" />
 <button className="btn btn-block">Login</button>
 <p className="message">
 Not Registered? <Link to="/signup">Create an account</Link>
@@ -23,4 +50,4 @@ Not Registered? <Link to="/signup">Create an account</Link>
             </div>
         </div>
     )
-}
+    }
