f9e47f10bcdaf408d6a9c408d6ff50f144f9e2e4
katicaaax <radosavljevic.katarina98@gmail.com>
2023-08-31 21:38:14 +0200

    Users CRUD in API. Prvi deo

--- react/src/pages/Users.jsx
+++ react/src/pages/Users.jsx
@@ -1,8 +1,22 @@
+import {useEffect, useState} from "react";
+import axiosClient from "../axios-client.js";
+import {Link} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
+
+
+
+
 export default function Users() {
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const {setNotification} = useStateContext()
+
+
 
     return (
         <div>
-      Users
+      
         </div>
     )
 }
