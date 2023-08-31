421507f7c77dfb35ed490ab10f548d4e0ba795ad
katicaaax <radosavljevic.katarina98@gmail.com>
2023-09-01 00:40:36 +0200

    UpdateUserRequest bug fix

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -3,13 +3,16 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class UpdateUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     *
+     * @return bool
      */
-    public function authorize(): bool
+    public function authorize()
     {
         return true;
     }
@@ -17,13 +20,13 @@ class UpdateUserRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, mixed>
      */
-    public function rules(): array
+    public function rules()
     {
         return [
             'name' => 'required|string|max:55',
-            'email' => 'required|email|unique:users,email,'.$this->id, //user mora da ima id
+            'email' => 'required|email|unique:users,email,'.$this->id,
             'password' => [
                 'confirmed',
                 Password::min(8)
