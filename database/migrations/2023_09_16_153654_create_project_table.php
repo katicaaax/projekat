aa0d6ebe53d66ece33d904c6cf9dab35c0ba54e2
katicaaax <radosavljevic.katarina98@gmail.com>
2023-09-16 17:37:39 +0200

    Kreiranje migracije i tabele Project

--- database/migrations/2023_09_16_153654_create_project_table.php
+++ database/migrations/2023_09_16_153654_create_project_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('services', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('project', function (Blueprint $table) {
+            //
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('services');
+        Schema::table('project', function (Blueprint $table) {
+            //
+        });
     }
 };
