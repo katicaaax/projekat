a9388e367fbe72cb86cae75d1ee8aa33f4da5e8b
katicaaax <radosavljevic.katarina98@gmail.com>
2023-09-16 20:49:11 +0200

    Rute drugi deo

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::post('/projects', ProjectController::class)->only(['store','destroy']);
+    Route::resource('/projects', ProjectController::class)->only(['store','destroy']);
     Route::apiResource('/users', UserController::class);
 });
 
