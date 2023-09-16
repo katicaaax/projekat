1caca96ff818eabb816d21658685c40ff8c942da
katicaaax <radosavljevic.katarina98@gmail.com>
2023-09-16 17:44:07 +0200

    Kreiranje Projekta i Rola

--- database/migrations/2023_09_16_154044_create_roles_table.php
+++ database/migrations/2023_09_16_154044_create_roles_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('project', function (Blueprint $table) {
-            //
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -21,8 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('project', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('roles');
     }
 };
