aa0f509208fddcf09335b4b5edb337ab1320ee01
katicaaax <radosavljevic.katarina98@gmail.com>
2023-09-16 19:42:24 +0200

    Dodavanje migracije za projekat - prioritet.

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'priority',
+    ];
 }
