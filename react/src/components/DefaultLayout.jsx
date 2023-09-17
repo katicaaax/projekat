42470a0d891f218b1eb907044b23573f97758eed
emanja-codes <necamark@gmail.com>
2023-09-17 19:39:55 +0200

    Navigacioni meni fix

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -31,7 +31,7 @@ export default function DefaultLayout() {
     <div id="defaultLayout">
       <aside>
         <Link to="/dashboard">Dashboard</Link>
-        {user.is_admin && <Link to="/users">Users</Link>}
+        {user.is_admin == 1 && <Link to="/users">Users</Link>}
         <Link to="/projects">Projects</Link>
         <Link to="/tasks">Tasks</Link>
       </aside>
